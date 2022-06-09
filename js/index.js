2c6b55442821759049d55309faafe9930d117c21
MarcosSilvaDev <marcos.dev1998@gmail.com>
2022-06-09 16:53:19 -0300

    Number guessing project

---
+++ js/index.js
@@ -0,0 +1,66 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit);
+
+let status = document.getElementById('status');
+let attempt = document.getElementById('attempt');
+let result = document.getElementById('result');
+
+const Guess = {
+    max: 10,
+    attemptsNumber: 0,
+    numberDrawn: function ramdomValue() {
+        return Math.round(Math.random() * this.max)
+    }
+}
+
+let numberDrawn = Guess.numberDrawn();
+
+function updateAttempt(attempt, value) {
+    attempt.innerHTML = "Tentativas: " + value
+};
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    let kick =  document.getElementById("kick").value;
+
+    if(kick) {
+        alert('Digite algum valor!')
+        ReadableStreamDefaultController;
+    };
+
+    updateAttempt(attempt, ++Guess.attemptsNumber);
+
+    if(numberDrawn == kick) {
+        playAgain();
+        status.innerHTML = "Parábens, você ganhou";
+        result.style.transition = "0.4s";
+        result.style.backgroundColor = "#37c978";
+        result.style.color = '#ffffff';
+        status.style.color = '#ffffff';
+        clear();
+    } else if(numberDrawn > kick) {
+        status.innerHTML = "O número é maior";
+        status.style.color = '#de4251';
+        clear();
+    } else if(numberDrawn < kick) {
+        status.innerHTML = "O número é menor";
+        status.style.color = '#de4251';
+        clear();
+    }
+
+
+}
+
+function playAgain() {
+    document.getElementById("btnRestart").style.display = "flex";
+
+}
+
+function restart() {
+    document.location.reload(true);
+}
+
+function clear() {
+    document.getElementById('kick').value = '';
+}
